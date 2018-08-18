c17f10af26479d1a9a5d4cb857f2036e24de0988
Josué Estévez Fernández <jestfer6@gmail.com>
2018-08-18 23:25:09 +0100

    value property added to input, tested

--- src/app/components/input/input.component.spec.ts
+++ src/app/components/input/input.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputComponent } from './input.component';
+import { By } from '@angular/platform-browser';
 
 describe('InputComponent', () => {
   let component: InputComponent;
   let fixture: ComponentFixture<InputComponent>;
+  let inputEl;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,6 +19,8 @@ describe('InputComponent', () => {
     fixture = TestBed.createComponent(InputComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    inputEl = fixture.debugElement.query(By.css('input')).nativeElement;
   });
 
   it('should create', () => {
@@ -24,6 +28,12 @@ describe('InputComponent', () => {
   });
 
   it('should have the following properties', () => {
-    // expect(component.value).not.toBe(null);
+    expect(component._value).not.toBe(null);
+  });
+
+  it('should set a value in nativeElement when component _value is set', () => {
+    component._value = 'jumanji';
+    fixture.detectChanges();
+    expect(inputEl.value).toEqual('jumanji');
   });
 });
